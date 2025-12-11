673303cdb73978a8e4017234ce51ce3bd8f544f1
Maryam Skaik <mariamskaik2003@gmail.com>
2025-12-11 15:48:44 +0200

    Update Shape.java

--- src/oop/Shape.java
+++ src/oop/Shape.java
@@ -1,22 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package oop;
 
 /**
- *
- * @author Maryam
+ * Abstract class representing a general shape.
+ * Subclasses must implement the draw() method.
  */
 public abstract class Shape {
-    public abstract void draw(); // { System.out.println("Drawing shape"); }
-    
+
+    public abstract void draw();
+
     public static void main(String[] args) {
+
         Circle c = new Circle();
-        Shape [] shapes = {c, new Triangle()};
-        
-        for(Shape s: shapes){
+        Shape[] shapes = { c, new Triangle() };
+
+        for (Shape s : shapes) {
             s.draw();
         }
     }
