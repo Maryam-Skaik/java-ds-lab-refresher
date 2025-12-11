7be785941887080f2fcc09d1b1b9af1899c72846
Maryam Skaik <mariamskaik2003@gmail.com>
2025-12-11 15:50:03 +0200

    Update Student.java

--- src/oop/Student.java
+++ src/oop/Student.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package oop;
 
 /**
- *
- * @author Maryam
+ * Simple Student class demonstrating encapsulation and constructors.
  */
 public class Student {
-    
+
     private String name;
     private int id;
 
@@ -18,16 +12,14 @@ public class Student {
         this.name = name;
         this.id = id;
     }
-    
-    //public Student(){}
 
     public String getName() { return name; }
     public int getId() { return id; }
-    
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    
+
     public static void main(String[] args) {
         Student s = new Student("Maryam", 1234);
     }
