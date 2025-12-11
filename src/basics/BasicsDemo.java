77f3c4e8687356a7cca6974e1b263034c9c2dfc3
Maryam Skaik <mariamskaik2003@gmail.com>
2025-12-11 15:47:49 +0200

    Update BasicsDemo.java

--- src/basics/BasicsDemo.java
+++ src/basics/BasicsDemo.java
@@ -1,41 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package java.ds.lab.refresher.basics;
 
 /**
- *
- * @author Maryam
+ * Demonstrates Java basic syntax: variables, loops, methods, and objects.
  */
 public class BasicsDemo {
+
     public static void main(String[] args) {
-        // Java Basics
+
+        // Primitive data types
         int x = 10;
         double y = 3.14;
         boolean flag = true;
 
+        // Simple if-statement
         if (x > 5) {
             System.out.println("x is greater than 5");
         }
-        
+
+        // Loop without initialization section
         int i = 0;
         for (; i < 3; ) {
             System.out.println("i = " + i);
             i++;
         }
-        
-        
 
-        System.out.println(sum(4, 6));  
-        System.out.println(new BasicsDemo().multiply(4, 6));  
+        // Calling static method
+        System.out.println(sum(4, 6));
+
+        // Calling instance method
+        System.out.println(new BasicsDemo().multiply(4, 6));
     }
 
+    // Static method example
     public static int sum(int a, int b) {
         return a + b;
     }
-    
+
+    // Instance method example
     public int multiply(int a, int b) {
         return a * b;
     }
