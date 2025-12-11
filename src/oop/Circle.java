9688cd16a11bba12617d51b9ccc0a0336a6656a7
Maryam Skaik <mariamskaik2003@gmail.com>
2025-12-11 15:49:01 +0200

    Update Circle.java

--- src/oop/Circle.java
+++ src/oop/Circle.java
@@ -1,15 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package oop;
 
 /**
- *
- * @author Maryam
+ * Circle implementation of the Shape class.
  */
 public class Circle extends Shape {
+
     @Override
-    public void draw() { System.out.println("Drawing circle"); }
+    public void draw() {
+        System.out.println("Drawing circle");
+    }
 }
