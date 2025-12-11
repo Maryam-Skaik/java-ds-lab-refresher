28d0d3fc9141b90fac266b4fbc5dbcd69a94a590
Maryam Skaik <mariamskaik2003@gmail.com>
2025-12-11 15:46:32 +0200

    Update ExceptionDemo.java

--- src/exceptions/ExceptionDemo.java
+++ src/exceptions/ExceptionDemo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exceptions;
 
 import java.io.FileNotFoundException;
@@ -10,32 +5,46 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /**
- *
- * @author Maryam
+ * Demonstrates basic usage of try-catch-finally blocks
+ * and working with checked exceptions such as FileNotFoundException.
  */
 public class ExceptionDemo {
-    
-    public static void main(String[] args) throws FileNotFoundException{
+
+    public static void main(String[] args) throws FileNotFoundException {
+
         try {
-            readFile();
+            readFile();   // Attempt to read a file
             int a = 10 / 10;
+
+            // Manually throwing an exception for demonstration purposes
             throw new ArithmeticException();
+
         } catch (ArithmeticException e) {
             System.out.println("Cannot divide by zero!");
-        }finally{
+
+        } finally {
+            // Always executed, whether an exception occurs or not
             System.out.println("done");
         }
     }
-    
+
+    /**
+     * Reads a file and handles common IO-related exceptions.
+     */
     public static void readFile() {
-        try{
-        FileReader fr = new FileReader("text.txt");
-        }catch(FileNotFoundException e){
+        try {
+            FileReader fr = new FileReader("text.txt");
+
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
-        }catch(IOException e){
-            
-        }catch(Exception e){
-            
+
+        } catch (IOException e) {
+            // Handles general input-output problems
+            System.out.println("I/O error occurred");
+
+        } catch (Exception e) {
+            // Handles all other unexpected exceptions
+            System.out.println("Unexpected error");
         }
     }
 }
