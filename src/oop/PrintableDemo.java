05a32a8fd302457cef0394c004391a4e0b53a5c7
Maryam Skaik <mariamskaik2003@gmail.com>
2025-12-11 15:48:25 +0200

    Update PrintableDemo.java

--- src/oop/PrintableDemo.java
+++ src/oop/PrintableDemo.java
@@ -1,27 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package oop;
 
 /**
- *
- * @author Maryam
+ * Interface demonstrating polymorphism through the print() method.
  */
 public interface PrintableDemo {
     void print();
 }
 
 class Book implements PrintableDemo {
-    public void print() { System.out.println("Printing a book..."); }
+    @Override
+    public void print() {
+        System.out.println("Printing a book...");
+    }
 }
 
-class Magazine implements PrintableDemo{
-
+class Magazine implements PrintableDemo {
     @Override
     public void print() {
         System.out.println("Printing a magazine...");
     }
-    
 }
